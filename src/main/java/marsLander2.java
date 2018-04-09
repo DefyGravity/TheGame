ac807cf5a8ac6d910454c76450aa5439168e71c9
michm <michm@DESKTOP-JEA82LO>
2018-04-09 01:31:53 -0400

    getting closer

--- src/main/java/marsLander2.java
+++ src/main/java/marsLander2.java
@@ -1,10 +1,11 @@
-import java.util.*;
 import java.awt.Rectangle;
 import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
-import java.io.*;
-import java.math.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -34,12 +35,21 @@ class Player {
             
         }
         Line2D destination = null;
+        Point2D landingZone = new java.awt.geom.Point2D.Double();
         // game loop
         while (true) {
             int X = in.nextInt();
             int Y = in.nextInt();
+            
             Point2D location = new Point2D.Double(Double.valueOf(X), Double.valueOf(Y));
-            if(destination == null){
+            int hSpeed = in.nextInt(); // the horizontal speed (in m/s), can be negative.
+            int vSpeed = in.nextInt(); // the vertical speed (in m/s), can be negative.
+            int fuel = in.nextInt(); // the quantity of remaining fuel in liters.
+            int rotate = in.nextInt(); // the rotation angle in degrees (-90 to 90).
+            int power = in.nextInt(); // the thrust power (0 to 4).
+            Double dPower = new Double(power);
+            if(destination == null) {
+            	
             	final Optional<Line2D> min = landingSpots.parallelStream().min((left, right) -> {
             		final int lcompare = Double.compare(left.getP1().distance(location), left.getP2().distance(location));
             		Point2D leftPt =null;
@@ -57,36 +67,40 @@ class Player {
             		}
             		final int compare = Double.compare(leftPt.distance(location), rightPt.distance(location));
             		
-					return compare;
+        			return compare;
             	});
             	destination = min.get();
-            	System.err.println(destination.getP1()+"**"+destination.getP2());
+            	
+            	landingZone = new Point2D.Double(destination.getX1()+ (destination.getX2()-destination.getX1())/2, destination.getX1());
+            			
             }
-            int hSpeed = in.nextInt(); // the horizontal speed (in m/s), can be negative.
-            int vSpeed = in.nextInt(); // the vertical speed (in m/s), can be negative.
-            int fuel = in.nextInt(); // the quantity of remaining fuel in liters.
-            int rotate = in.nextInt(); // the rotation angle in degrees (-90 to 90).
-            int power = in.nextInt(); // the thrust power (0 to 4).
-            double radians = Math.toRadians(Double.valueOf(rotate));
-            Point2D midPoint = new Point2D.Double(destination.getX1()+ (destination.getX2()-destination.getX1()/2), destination.getX1());
             
-            final double distance = location.distance(midPoint);
-            final double theta = Math.acos((location.getY()-midPoint.getY())/distance);
-            final Double degrees = -(Math.toDegrees(theta)-90);
-            if(vSpeed <= -35){
-                power = 4;
-            }else if(vSpeed <= -20){
-                power = 3;
-            }else {
-                power = 0;
-            }
+            final double distance = location.distance(landingZone);
+            double desiredVelocityX = (location.getX() - landingZone.getX()) * 4 /distance;
+            double desiredVelocityY = (location.getY() - landingZone.getY()) * 3.711 /distance;
+            double deltaX = desiredVelocityX - hSpeed;
+            double deltaY = desiredVelocityY - vSpeed;
+            double diffSize = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+            double accelerateX = 4 * deltaX / diffSize;
+            double accelerateY = 3.711 * deltaY / diffSize;
+            Double degrees = Math.toDegrees(Math.tan(accelerateY/accelerateX));
+//            final double theta = Math.acos((location.getY()-landingZone.getY())/distance);
+//            final Double degrees = -(Math.toDegrees(theta)-90);
+//            if(vSpeed <= -35){
+//                power = 4;
+//            }else if(vSpeed <= -20){
+//                power = 3;
+//            }else {
+//                power = 0;
+//            }
 
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
-
-
+            
+System.err.println(desiredVelocityX +" "+desiredVelocityY+"\t:\t"+landingZone);
             // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
-            System.out.println(degrees.intValue() +" "+power);
+            dPower = Math.sqrt(Math.pow(accelerateX,2)+Math.pow(accelerateY,2));
+            System.out.println(degrees.intValue() +" "+dPower.intValue());
         }
     }
 }
