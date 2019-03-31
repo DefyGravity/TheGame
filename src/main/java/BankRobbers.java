f99a3bfe91c5231948a722a2a6a712314420aa8f
michm <michm@DESKTOP-JEA82LO>
2019-03-31 01:46:01 -0400

    bankrobbers start

--- src/main/java/BankRobbers.java
+++ src/main/java/BankRobbers.java
@@ -12,14 +12,18 @@ class Solution {
         Scanner in = new Scanner(System.in);
         int R = in.nextInt();
         int V = in.nextInt();
+        Double time = 0.0;
+        System.err.println(R+","+V);
+        List<Double> combos = new ArrayList<>();
         for (int i = 0; i < V; i++) {
             int C = in.nextInt();
             int N = in.nextInt();
+            combos.add(Math.pow(10, N)*Math.pow(5, C-N));
         }
-
+        
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
-
-        System.out.println("1");
+        
+        System.out.println(time.intValue());
     }
 }
